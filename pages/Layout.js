79063e6cad7c85b108f2d9e3be5faa09b0ed5946
Heroboycloud Akindel <shakiratakindel@gmail.com>
2023-10-10 23:39:52 +0100

    wahala don bust oo

--- pages/Layout.js
+++ pages/Layout.js
@@ -14,7 +14,10 @@ export default function RootLayout({ children }) {
     content="https://mini-diffusion.vercel.app/api/og"
   />
 <meta name="description" content="Developed by Akindel, Mini diffusion app" />
-        <link rel="icon" href="/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
 <title>
 Awesome AI || Mini-diffusion by Akindel
 </title>
