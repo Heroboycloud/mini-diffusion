8b8b099080c982ce8b1635016e842bdeed8eb9c2
Heroboycloud Akindel <shakiratakindel@gmail.com>
2023-10-14 20:42:34 +0100

    deleted:    pages/admin/index.page.js 	new file:   pages/dashboard/index.js

---
+++ pages/dashboard/index.js
@@ -0,0 +1,16 @@
+'use client'
+import React from "react";
+import { useAuthContext } from "../context/AuthContext";
+import { useRouter } from "next/navigation";
+function Page() {
+    const { user } = useAuthContext()
+    const router = useRouter()
+
+    React.useEffect(() => {
+        if (user == null) router.push("/signin")
+    }, [user])
+
+    return (<h1>Welcome to the website</h1>);
+}
+
+export default Page;
