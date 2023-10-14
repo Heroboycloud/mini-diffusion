779823e8f9e8b8a0ecac778b92d5b9a54e13565a
Heroboycloud Akindel <shakiratakindel@gmail.com>
2023-10-14 19:09:21 +0100

    modified:   next.config.js 	renamed:    pages/404.js -> pages/404.page.js 	renamed:    pages/_app.js -> pages/_app.page.js 	renamed:    pages/admin/index.js -> pages/admin/index.page.js 	renamed:    pages/index.js -> pages/index.page.js 	renamed:    pages/lama.js -> pages/lama.page.js 	renamed:    pages/lordlama.js -> pages/lordlama.page.js 	renamed:    pages/signin/index.js -> pages/signin/index.page.js 	renamed:    pages/signup/index.js -> pages/signup/index.page.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: [ 'js'],
+  pageExtensions: [ 'page.js'],
   reactStrictMode: true,
   images: {
     remotePatterns: [
