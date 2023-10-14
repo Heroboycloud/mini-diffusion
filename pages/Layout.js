a77be8592deb813c94b227ff30b6c3809babfc7a
Heroboycloud Akindel <shakiratakindel@gmail.com>
2023-10-14 12:10:55 +0100

    modified:   pages/Layout.js 	modified:   pages/context/AuthContext.js

--- pages/Layout.js
+++ pages/Layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 <link rel="manifest" href="/site.webmanifest" />
 <title>
-Awesome AI || Mini-diffusion by Akindel
+Awesome AI || Mini-diffusion by Akindel the dev
 </title>
       </head>
       <body>
