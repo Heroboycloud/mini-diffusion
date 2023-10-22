e7a72591897aae292586006048e6fc1854e0966d
Heroboycloud Akindel <shakiratakindel@gmail.com>
2023-10-22 20:29:28 +0100

    god huh

--- pages/learn/jokes/index.page.js
+++ pages/learn/jokes/index.page.js
@@ -1,23 +1,12 @@
-export const getStaticProps= async () =>{
-const res = await fetch('http://localhost:3000/jokes.json')
-  const jokes = await res.json()
-  return { props: { jokes } }
+'use client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
-}
-export default function Page({ jokes }) {
+export default function Home(){
+  const { push } = useRouter();
 
-return (
-<div>
-{jokes.map(function(joke,key){
-return (
-<div key={key}>
-<h4>{joke.setup}</h4>
-<p>{joke.punchline}</p>
-</div>
-);
-
-})}
-</div>
-
-);
-}
+  useEffect(() => {
+     push('/learn/jokes/1');
+  }, []);
+  return <p></p>;
+};
