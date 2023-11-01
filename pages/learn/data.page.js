f4710a4c0da484f9dc998cf9c9cfd66115d5510b
Heroboycloud Akindel <shakiratakindel@gmail.com>
2023-11-01 18:50:07 +0100

    modified:   pages/learn/data.page.js

--- pages/learn/data.page.js
+++ pages/learn/data.page.js
@@ -18,10 +18,33 @@ function Profile() {
   if (!data) return <p>No profile data</p>
  
   return (
-    <div>
-      <h1>{data.user}</h1>
-      <p>{data.bio}</p>
-    </div>
+    <div class="bg-gray-50 p-10"> 
+    <div class="mb-10">
+    <div class="flex justify-center bg-white-300 shadow-2xl ">
+<div class="mx-4 self-center text-center p-10">
+      <h1 class="text-blue-700 text-6xl font-bold text-center self-center hover:text-slate-300 ">{data.user}</h1>
+      <h2 class="text-3xl font-semibold text-blue-300"> { data.bio } </h2>
+      <button class="my-4 px-4 py-2 border-2 border-blue-300 rounded-lg text-white bg-blue-900"> Learn More </button>
+    </div></div></div></div>
   )
 }
-export default Profile;
+function Projects(){
+
+return (
+<div class="grid grid-cols-2  gap-4"> 
+<div class="border text-slate-400 font-semibold p-5 bg-green-300 text-center shadow-2xl">Java Apps</div> <div class="border bg-gray-300 text-center shadow-2xl">Mini-Diffusion</div> <div class="border bg-gray-300 text-center shadow-xl">C</div> <div class="border bg-gray-300 text-center shadow-xl">Android web dev</div> </div>
+
+);
+
+}
+export default function Full(){
+
+return (
+<>
+<Profile />
+<Projects />
+</>
+);
+
+
+};
