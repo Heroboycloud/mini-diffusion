6c79eeddcce4ee87d8c4ba5e5917690d0c92b509
Heroboycloud Akindel <shakiratakindel@gmail.com>
2023-11-11 16:13:06 +0100

    modified:   package.json 	new file:   pages/config/firebaseApp.config.js 	new file:   pages/config/firebaseAuthUi.config.js 	new file:   pages/login.page.js

---
+++ pages/login.page.js
@@ -0,0 +1,53 @@
+// Next JS related
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+// Firebase related
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import { auth, firebase_app } from './config/firebaseApp.config.js';
+
+import { uiConfig } from './config/firebaseAuthUi.config.js';
+
+/* Components
+import Logo from '../components/elements/Logo';
+import Card from '../components/elements/Card';
+import Error from '../components/elements/Error';
+import Loading from '../components/elements/Loading';
+*/
+/* Styles
+import LoginStyle from '../styles/Login.module.css';
+*/
+
+export default function Login() {
+    const [user, loading, error] = useAuthState(auth);
+    const router = useRouter();
+
+    if (loading) return "Loading.."
+    else if (error) return "error.."
+
+    else if (user) {
+        // user is already logged in, redirect to home page
+        router.push('/');
+    }
+
+    const authConfig = uiConfig(firebase_app);
+
+    return (
+        <>
+            <Head>
+                <title>CodeBlog | LogIn</title>
+            </Head>
+            <div className={LoginStyle.login}>
+                <div>
+                    <div className={LoginStyle.loginTitleContainer}>
+                        <h1>Log In to</h1>
+                        <Logo />
+                    </div>
+                    <StyledFirebaseAuth uiConfig={authConfig} firebaseAuth={auth} />
+                </div>
+            </div>
+        </>
+    )
+}
