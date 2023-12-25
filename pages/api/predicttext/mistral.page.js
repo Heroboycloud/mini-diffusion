26aa88f55d75de3ca9e313617950c8123c6272a8
Heroboycloud Akindel <shakiratakindel@gmail.com>
2023-12-25 17:38:53 +0100

    deleted:    pages/api/predictlama/[id].page.js 	renamed:    pages/api/predictlama/index.page.js -> pages/api/predicttext/lama.page.js 	new file:   pages/api/predicttext/mistral.page.js 	new file:   pages/tools/textgen/create.page.js

--- pages/api/predicttext/mistral.page.js
+++ pages/api/predicttext/mistral.page.js
@@ -13,12 +13,19 @@ export default async function handler(req, res) {
 
   const prediction = await replicate.predictions.create({
     // Pinned to a specific version of Stable Diffusion
-    // See https://replicate.com/stability-ai/sdxl
-    version: "6282abe6a492de4145d7bb601023762212f9ddbbe78278bd6771c8b3b2f2a13b",
+    // Mistral
+    version: "2b56576fcfbe32fa0526897d8385dd3fb3d36ba6fd0dbe033c72886b81ade93e",
 input: {
-    //system_prompt: "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.",
-     max_length: 1000,
-    prompt: req.body.prompt },
+      top_k: 50,
+      top_p: 0.9,
+      prompt: req.body.prompt,
+      temperature: 0.6,
+      max_new_tokens: 1024,
+      prompt_template: "<s>[INST] {prompt} [/INST]",
+      presence_penalty: 0,
+      frequency_penalty: 0
+    }
+
     // This is the text prompt that will be submitted by a form on the frontend
   });
 
---
+++ pages/tools/textgen/create.page.js
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import Head from "next/head";
+import Image from "next/image";
+
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+export default function Home() {
+  const [prediction, setPrediction] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("/api/predicttext/mistral", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: e.target.prompt.value,
+      }),
+    });
+    let prediction = await response.json();
+    if (response.status !== 201) {
+      setError(prediction.detail);
+      return;
+    }
+    setPrediction(prediction);
+
+    while (
+      prediction.status !== "succeeded" &&
+      prediction.status !== "failed"
+    ) {
+      await sleep(4000);
+      const response = await fetch("/api/predictlama/" + prediction.id);
+      prediction = await response.json();
+      if (response.status !== 200) {
+        setError(prediction.detail);
+        return;
+      }
+      console.log({ prediction });
+      setPrediction(prediction);
+    }
+  };
+
+  return (
+    <div className="container max-w-2xl mx-auto p-5">
+      <Head>
+        <title>LordLama ask question</title>
+      </Head>
+
+      <h1 className="py-6 text-center font-bold text-2xl">
+        Ask any question from {" "}
+        <a href="https://replicate.com/stability-ai/sdxl?utm_source=project&utm_project=getting-started">
+          LordLama
+        </a>
+      </h1>
+
+      <form className="w-full flex" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="flex-grow"
+          name="prompt"
+          placeholder="Enter a prompt to display an image"
+        />
+        <button className="button" type="submit">
+          Go!
+        </button>
+      </form>
+
+      {error && <div>{error}</div>}
+
+      {prediction && (
+        <>
+          {prediction.output && (
+            <div className="border border-slate-300 hover:border-indigo-300 mt-5 overflow-auto">
+             <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+        <dt class="text-sm font-medium leading-6 text-gray-900">{prediction.input.prompt}</dt>
+        <p class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0 overflow-auto">
+       {prediction.output}
+</p>
+      </div>
+            </div>
+          )}
+          <p className="py-3 text-sm opacity-50">status: {prediction.status}</p>
+        </>
+      )}
+    </div>
+  );
+}
